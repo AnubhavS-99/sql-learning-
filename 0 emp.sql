00978c2396f21a51bf65efe6fb56c5be1e69c96f
AnubhavS-99 <anubhavsingh2236@gmail.com>
2025-07-11 11:05:18 +0530

    Add files via upload

---
+++ 0 emp.sql
@@ -0,0 +1,17 @@
+ CREATE TABLE emp_details2(
+         EMP_id SERIAL PRIMARY KEY,
+		 DEPARTMENT VARCHAR(50),
+		 SALARY NUMERIC(10,2),
+		 HIRED_DATE date,
+		 LOCATION VARCHAR(50)
+		 
+
+
+
+ );
+
+
+ SELECT*FROM emp_details;
+
+	
+ 
